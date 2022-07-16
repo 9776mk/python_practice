0508c898e022cc6c98c55202093323159a3688b1
9776mk <9776mk@gmail.com>
2022-07-16 14:56:13 +0900

    0716 백준

---
+++ 백준/BOJ_10818.py
@@ -0,0 +1,4 @@
+N = int(input())
+B = list(map(int, input().split()))
+
+print(min(B), max(B))
--- 백준/BOJ_1110.py
+++ 백준/BOJ_1110.py
@@ -1,12 +1,38 @@
 N = input()
-new_N = ''
+n_first = N
+temp_n = 0
 cnt = 0
+n_is_equal = False
 
 
-while True:
-    if int(N) < 10:
+while n_is_equal != True:
+    if len(N) == 1: #N이 한 글자인 경우
         N = '0' + N
-        print(N)
-        break
-    else:
-        new_N = 10
+        cnt += 1
+        if N == n_first:
+            n_is_equal = True
+            break
+
+        else:
+            continue
+    
+    else: # len(N) ==2, 2글자
+        temp_n = str(int(N[0]) + int(N[1]))
+        cnt += 1
+        if len(temp_n) == 1:
+            temp_n = '0' + temp_n
+            N = N[1] + temp_n[1]
+            if N == n_first:
+                n_is_equal = True
+                break
+            else:
+                continue
+        else: # 2글자 이면
+            N = N[1] + temp_n[1]
+            if N == n_first:
+                n_is_equal = True
+                break
+            else:
+                continue
+
+print(cnt)
