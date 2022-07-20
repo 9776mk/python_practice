d418521dd71145a0e54e4bfc4781b593b81c5095
9776mk <9776mk@gmail.com>
2022-07-20 23:22:03 +0900

    swea d1,d2 폴더 생성 및 codeup 폴더 이동

--- SWEA/D2/swea_1284.py
+++ SWEA/D2/swea_1284.py
@@ -8,8 +8,11 @@ for test_case in range(1,T+1):
         cost_B = Q
     else:
         cost_B = Q + (W-R)*S
-    
+    # cost_B = Q if W <= R else Q + (W-R) * S
+
+
     cost = min(cost_A, cost_B)
     print(f'#{test_case} {cost}')
 
+    # print(f'#{test_case} {min(cost_A, cost_B)}') #제일 간단한 풀이!
     # print(f'#{test_case} {cost_A}' if cost_A < cost_B else f'#{test_case} {cost_B}')
