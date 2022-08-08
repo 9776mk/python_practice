264d3f01fe3381727d7c97e67cb16e61b1b34ded
9776mk <9776mk@gmail.com>
2022-08-09 01:21:45 +0900

    0808 문제

--- 백준/BOJ_1543.py
+++ 백준/BOJ_1543.py
@@ -3,23 +3,25 @@ word_ = input()
 
 str_len = len(str_)
 word_len = len(word_)
+
 idx_ = 0
 cnt = 0
 
-print(str_len, word_len, idx_, cnt)
-
-
 # 0부터 시작해서
-while True:
-    if word_ == str_[idx_:word_len]:
-        print(str_[idx_:word_len])
+# 
+while idx_ <= str_len - word_len:
+    #print(str_[idx_:idx_+ word_len])
+    if str_[idx_:idx_+ word_len] == word_ :
         cnt += 1
         idx_ += word_len
     else:
-        print(str_[idx_:word_len])
         idx_ += 1
 
-    if idx_ == str_len - word_len + 1:
-        break
-
 print(cnt)
+
+
+'''
+a = input()
+b = input()
+print(a.count(b))
+'''
--- 백준/BOJ_2798.py
+++ 백준/BOJ_2798.py
@@ -2,9 +2,10 @@ N, M = map(int, input().split())
 list_ = list(map(int, input().split()))
 
 sum_ = 0
+
 for i in range(N-2):
     for j in range(i+1, N-1):
-        for k in range(i+2, N):
+        for k in range(j+1, N):
             sum_tmp = 0
             sum_tmp = list_[i] + list_[j] + list_[k]
             if sum_ < sum_tmp < M:
@@ -12,5 +13,9 @@ for i in range(N-2):
             elif sum_tmp == M:
                 sum_ = sum_tmp
                 break
+        if sum_ == M:
+            break
+    if sum_ == M:
+            break
 
 print(sum_)
